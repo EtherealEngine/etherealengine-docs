9cdfda264c2e056debd18ec57ef1dde8671a3a4d
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-01-04 23:59:22 +0100

    [all] Micro-sites strategy restructure (#104)
    * fmt: Full website restructure: Micro-sites strategy
    
    * git: Delete t.js
    
    * chg: Add syntax highlighting for additional languages in markdown
    
    * fmt: Cleanup the config file. behavegraph renamed to visualscript
    
    * fix: Routing issues with temporary files in the learning sites
    
    * chg: Disabled the creators/developers/examples navbars for public
    
    * chg: Fixes to the hero banner styling

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,24 +6,30 @@ const dotenv = require('dotenv').config({
   path: appRootPath.path + '/.env.local'
 })
 
-const config: Config = {
-  title: 'etherealengine',
-  tagline:     'An open source solution for hosting, creating and developing immersive social spaces, built on top of WebXR, React & Feathers.',
-  favicon: 'img/favicon.ico',
+// General Configuration
+const orgTitle         = 'Ethereal Engine'
+const orgName          = 'etherealengine'
+const projectName      = 'etherealengine-docs'
+const editURL          = `https://github.com/EtherealEngine/${projectName}/blob/master/`
+const siteURL          = 'https://etherealengine.github.io'
+const siteTitle        = `${orgTitle} Documentation`
+const siteTagline      = `${orgTitle} is an open source solution for hosting, creating and developing immersive social spaces.`
+const siteIcon         = 'img/favicon.ico'
+const visualScriptName = 'Behave Graph'
 
-  // Set the production url of your site here
-  url: 'https://etherealengine.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/etherealengine-docs/',
+// Docusaurus Configuration
+const config: Config = {
+  title: siteTitle,
+  tagline: siteTagline,
+  favicon: siteIcon,
+  url: siteURL,  // Set the production url of your site here
+  baseUrl: `/${projectName}/`, // The /<baseUrl>/ pathname under which your site is served. For GitHub pages it is often '/<projectName>/'
+  organizationName: orgName,   // GitHub pages organization. Usually your GitHub org/user name. Not needed when not using GitHub pages.
+  projectName: projectName,    // GitHub pages project. Usually your repo name. Not needed when not using GitHub pages.
+
+  // Broken Links behavior
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'etherealengine', // Usually your GitHub org/user name.
-  projectName: 'etherealengine-docs', // Usually your repo name.
-
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -38,11 +44,10 @@ const config: Config = {
       'classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.ts'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          exclude: ['**/_*.{js,jsx,ts,tsx,md,mdx}'],
-          editUrl: 'https://github.com/EtherealEngine/etherealengine-docs/blob/master/'
+          exclude: ['**/_*.{js,jsx,ts,tsx,md,mdx}', '**/_partials/**'],
+          editUrl: editURL, // Remove this to remove the "edit this page" links.
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
@@ -52,12 +57,14 @@ const config: Config = {
   ],
 
   themeConfig: {
+    colorMode: { defaultMode: 'dark' },
     docs: {
       sidebar: {
         autoCollapseCategories: true,
         hideable: true
       }
     },
+    blog: null, // Explicitly disable the default blog plugin
     algolia: {
       appId: "N5OIDFHT9B",
       apiKey: "e27783cb4ed9bfc6a011eb7026868c20",
@@ -71,26 +78,19 @@ const config: Config = {
         src: 'img/logo.svg'
       },
       items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left'
-        },
-        {
-          href: 'https://etherealengine.github.io/etherealengine-docs/typedoc',
-          label: 'API',
-          position: 'left'
-        },
-        {
-          href: 'https://etherealengine.org/',
-          label: 'Ethereal Engine',
-          position: 'right'
-        },
-        {
-          type: 'localeDropdown',
-          position: 'right',
-        },
+        // TODO: Uncomment when the learning sites are presentable
+        //{ label: 'Creators', position: 'left', to: 'creator' },
+        //{ label: 'Developers', position: 'left', type: 'dropdown',
+        //  items: [
+        //    { label: visualScriptName, to: 'developer/visualscript' },
+        //    { label: 'Typescript', to: 'developer/typescript'  },
+        //  ]
+        //},
+        //{ label: 'Examples', position: 'left', to: 'examples', },  // TODO: Uncomment when the examples pages is ready
+        { label: 'Manual', position: 'left', to: 'manual', },
+        //{ label: 'API', position: 'left', href: 'https://etherealengine.github.io/etherealengine-docs/typedoc' },
+        { label: 'Ethereal Engine', position: 'right',  href: 'https://etherealengine.org/' },
+        { type: 'localeDropdown', position: 'right' },
       ]
     },
 
@@ -100,52 +100,32 @@ const config: Config = {
         {
           title: 'Social',
           items: [
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/xr_engine'
-            },
-            {
-              label: 'Facebook',
-              href: 'https://www.facebook.com/xrengine/'
-            },
-            {
-              label: 'Discord',
-              href: 'https://discord.gg/xrf'
-            }
+            { label: 'Twitter', href: 'https://twitter.com/xr_engine' },
+            { label: 'Facebook', href: 'https://www.facebook.com/xrengine/' },
+            { label: 'Discord', href: 'https://discord.gg/xrf' }
           ]
         },
         {
           title: 'Resources',
           items: [
-            {
-              label: 'Github',
-              href: 'https://github.com/etherealengine/etherealengine'
-            },
-            {
-              label: 'Npm',
-              href: 'https://www.npmjs.com/search?q=%40etherealengine'
-            }
+            { label: 'Github', href: 'https://github.com/etherealengine/etherealengine' },
+            { label: 'Npm', href: 'https://www.npmjs.com/search?q=%40etherealengine' }
           ]
         },
         {
           title: 'More',
           items: [
-            {
-              label: 'Ethereal Engine',
-              href: 'https://www.etherealengine.org/'
-            },
-            {
-              label: 'Open Collective',
-              href: 'https://opencollective.com/etherealengine'
-            }
+            { label: 'Ethereal Engine', href: 'https://www.etherealengine.org/' },
+            { label: 'Open Collective', href: 'https://opencollective.com/etherealengine' }
           ]
         }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Ethereal Engine.`
+      copyright: `Copyright © ${new Date().getFullYear()} Ethereal Engine.`,
     },
   prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['diff','yaml','toml','bash','powershell','c','cpp','python'],
     },
   } satisfies Preset.ThemeConfig,
 };
