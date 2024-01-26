43e5f5394022a2b9ba6fd5472edcd590844e872b
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-01-26 21:09:37 +0100

    [general] Revamp darkmode styling, create the mvp Landing Page and sketch the main Landing Page (#107)
    * docs intro pass 1
    
    * fmt: Moved the roles-guidelines into the temporary overview page
    
    * new: TailwindCSS and Docusaurus v3.1.0
    
    * new: Landing page tsx sketch
    
    * chg: Docusaurus ignore for _ files/folders inside pages
    
    * chg: Docusaurus configuration values imported from website.config.ts
    
    * new: New landing page sketch
    
    * chg: Implement Data.props drilling and Fix tailwind processing error
    
    * fmt: Small cleanup of the landing page sketch
    
    * new: Add GitHub icon svg to custom.css
    
    * chg: Multiple configuration improvements/changes
    
    * chg: Increment TOC maximum heading level registered to 5 (default 3)
    
    * chg: Big color and Theming changes
    
    * chg: Fix Github icon link to point to the main repository (not docs)
    
    * fmt: Better links management in the unified website config file
    
    * fix: Add font link to the docusaurus headTags list
    
    * fmt: Comment darkmode ifm bg color, as reminder for the !important tag
    
    * chg: Swap info/tip admonition colors
    
    * new: Simplified Landing Page (MVP version)
    
    * chg: Make PersonaCTAs clickable for mvp+normal landing
    
    * new: Hover behavior for persona Cards and CTAs
    
    * chg: Disable light mode for now. It breaks with the new wip CSS theme
    
    * fmt: Remove css nesting for admonitions
    
    * fmt: Add css nesting to fix the colors until reviewed
    
    * Fix colors, fix onClick
    
    * chg: Add transition-all to landing page hover properties
    
    * fix: Landing page links to the developer microsites
    
    * chg: Responsive landing page phase0
    
    * chg: Add links to the Persona cards
    
    * chg: Add basic reponsiveness for phone/lg
    
    ---------
    
    Co-authored-by: Anselm Hook <anselm@gmail.com>
    Co-authored-by: Daniel Belmes <3631206+DanielBelmes@users.noreply.github.com>
    Co-authored-by: Daniel Belmes <danielbelmes@ymail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,4 +1,5 @@
 import {themes as prismThemes} from 'prism-react-renderer';
+import { ethereal } from './src/themes/code/ethereal.ts';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import * as appRootPath from 'app-root-path'
@@ -13,17 +14,16 @@ const config: Config = {
   tagline: cfg.site.tagline,
   favicon: cfg.site.icon,
   url: cfg.site.url,                      // Set the production url of your site here
-  baseUrl: `/${cfg.github.projectName}/`, // The /<baseUrl>/ pathname under which your site is served. For GitHub pages it is often '/<projectName>/'
-  organizationName: cfg.org.name,         // GitHub pages organization. Usually your GitHub org/user name. Not needed when not using GitHub pages.
-  projectName: cfg.github.projectName,    // GitHub pages project. Usually your repo name. Not needed when not using GitHub pages.
+  baseUrl: cfg.site.baseURL,              // The /<baseUrl>/ pathname under which your site is served. For GitHub pages it is often '/<projectName>/'
+  organizationName: cfg.github.username,  // GitHub pages organization. Usually your GitHub org/user name. Not needed when not using GitHub pages.
+  projectName: cfg.github.docsName,       // GitHub pages project. Usually your repo name. Not needed when not using GitHub pages.
 
   // Broken Links behavior
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+  // Even if you don't use internationalization, you can use this field to set useful metadata like html lang.
+  // For example, if your site is Chinese, you may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'/*, 'zh-cn'*/],
@@ -53,7 +53,12 @@ const config: Config = {
   ],
 
   themeConfig: {
-    colorMode: { defaultMode: 'dark' },
+    image: cfg.meta.image,
+    colorMode: {
+      defaultMode: 'dark',              // Default to darkmode, as it fits the engine brand identity better
+      disableSwitch: true,              // TODO: Reenable light mode. Disabled until its designed is properly developed
+      respectPrefersColorScheme: false, // Make it so that we decide the theme, not the browser
+    },
     docs: {
       sidebar: {
         autoCollapseCategories: true,
@@ -64,63 +69,42 @@ const config: Config = {
     algolia: {
       appId: "N5OIDFHT9B",
       apiKey: "e27783cb4ed9bfc6a011eb7026868c20",
-      indexName: "etherealengine",
+      indexName: cfg.org.username,
       contextualSearch: true,
     },
     navbar: {
-      title: 'Ethereal Engine',
-      logo: {
-        alt: 'Ethereal Engine Logo',
-        src: 'img/logo.svg'
-      },
+      title: cfg.site.title,
+      logo: cfg.org.logo,
       items: [
+        { type: 'search', position: cfg.navbar.search.position, className: cfg.navbar.search.css },
         // TODO: Uncomment when the learning sites are presentable
-        //{ label: 'Creators', position: 'left', to: 'creator' },
-        //{ label: 'Developers', position: 'left', type: 'dropdown',
+        //{ label: 'Creators', position: cfg.navbar.menu.position, to: 'creator' },
+        //{ label: 'Developers', position: cfg.navbar.menu.position, type: 'dropdown',
         //  items: [
-        //    { label: visualScriptName, to: 'developer/visualscript' },
+        //    { label: cfg.VisualscriptName, to: 'developer/visualscript' },
         //    { label: 'Typescript', to: 'developer/typescript'  },
         //  ]
         //},
-        //{ label: 'Examples', position: 'left', to: 'examples', },  // TODO: Uncomment when the examples pages is ready
-        { label: 'Manual', position: 'left', to: 'manual', },
-        //{ label: 'API', position: 'left', href: 'https://etherealengine.github.io/etherealengine-docs/typedoc' },
-        { label: 'Ethereal Engine', position: 'right',  href: 'https://etherealengine.org/' },
-        { type: 'localeDropdown', position: 'right' },
+        //{ label: 'Examples', position: cfg.navbar.menu.position, to: 'examples', },  // TODO: Uncomment when the examples pages is ready
+        { label: 'Manual', position: cfg.navbar.menu.position, to: 'manual', },
+        //{ label: cfg.typedoc.label, position: cfg.navbar.menu.position, href: cfg.typedoc.url },
+        { "aria-label": "GitHub", position: cfg.navbar.menu.position, className: "header-github-link", href: cfg.github.projectURL, },
+        //{ label: cfg.org.title, position: cfg.navbar.menu.position,  href: cfg.org.url.orgsite },
+        //{ type: 'localeDropdown', position: 'right' },
       ]
     },
-
     footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Social',
-          items: [
-            { label: 'Twitter', href: 'https://twitter.com/xr_engine' },
-            { label: 'Facebook', href: 'https://www.facebook.com/xrengine/' },
-            { label: 'Discord', href: 'https://discord.gg/xrf' }
-          ]
-        },
-        {
-          title: 'Resources',
-          items: [
-            { label: 'Github', href: 'https://github.com/etherealengine/etherealengine' },
-            { label: 'Npm', href: 'https://www.npmjs.com/search?q=%40etherealengine' }
-          ]
-        },
-        {
-          title: 'More',
-          items: [
-            { label: 'Ethereal Engine', href: 'https://www.etherealengine.org/' },
-            { label: 'Open Collective', href: 'https://opencollective.com/etherealengine' }
-          ]
-        }
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} ${cfg.org.name}.`,
+      logo: { ...cfg.org.logo, width: cfg.footer.iconSize, height: cfg.footer.iconSize },
+      links: [...cfg.footer.links],
+      copyright: cfg.footer.copyright,
+    },
+    tableOfContents: {
+      minHeadingLevel: 2, // default 2
+      maxHeadingLevel: 5, // default 3
     },
   prism: {
       theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
+      darkTheme: ethereal.dark,
       additionalLanguages: ['diff','yaml','toml','bash','powershell','c','cpp','python'],
     },
   } satisfies Preset.ThemeConfig,
@@ -137,6 +121,12 @@ const config: Config = {
         },
       };
     },
+  ],
+  headTags: [
+    // Add Hanken Grotesk font globally to the site
+    { tagName: 'link', attributes: { rel:"preconnect", href: "https://fonts.googleapis.com" } },
+    { tagName: 'link', attributes: { rel:"preconnect", href: "https://fonts.gstatic.com", crossorigin: 'true' } },
+    { tagName: 'link', attributes: { rel:"stylesheet", href: "https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" } },
   ],
 };
 
--- website.config.ts
+++ website.config.ts
@@ -5,40 +5,135 @@
  */
 
 /**
+ * @description General Variables
+ */
+const OrgFullName     = 'Ethereal Engine'
+const OrgName         = 'etherealengine'
+const ProjectFullName = 'Ethereal Engine'
+const ProjectName     = 'etherealengine'
+const GHUsername      = 'EtherealEngine'
+const GHDocsName      = `${ProjectName}-docs`
+const GHDocsURL       = `https://github.com/${GHUsername}/${GHDocsName}`
+const GithubURL       = `https://github.com/${GHUsername}/${ProjectName}`
+const SiteURL         = `https://${OrgName}.github.io`
+const BaseURL         = `/${GHDocsName}/`
+const Links           = {
+  twitter             : 'https://twitter.com/xr_engine',
+  facebook            : 'https://www.facebook.com/xrengine/',
+  discord             : 'https://discord.gg/xrf',
+  npm                 : 'https://www.npmjs.com/search?q=%40etherealengine',
+  openCollective      : 'https://opencollective.com/etherealengine',
+  github              : GithubURL,
+  orgSite             : `https://${OrgName}.org/`,
+  comSite             : `https://${OrgName}.com/`,
+}
+
+/**
  * @description Organization Options
  */
 export const org = {
-  title : 'Ethereal Engine',
-  name  : 'etherealengine'
+  title         : OrgFullName,
+  username      : OrgName,
+  logo          : {
+    alt         : `${OrgFullName} Logo`,
+    src         : 'img/logo.svg',
+  },
+  url           : {
+    orgsite     : Links.orgSite,
+    comsite     : Links.comSite,
+  },
+  project       : {
+    name        : ProjectFullName,
+    description : `${ProjectFullName} is an open source solution for hosting, creating and developing immersive social spaces.`,
+  },
 }
 
 /**
  * @description General website options
  */
 export const site = {
-  url     : `https://${org.name}.github.io`,
-  title   : `${org.title} Documentation`,
-  tagline : `${org.title} is an open source solution for hosting, creating and developing immersive social spaces.`,
-  icon    : 'img/favicon.ico',
+  url         : SiteURL,
+  baseURL     : BaseURL,
+  fullURL     : SiteURL+BaseURL,
+  title       : `${org.project.name} Docs`,
+  tagline     : org.project.description,
+  icon        : 'img/favicon.ico',
+  description : `Learn how to use ${org.project.name}.`,
 }
 
 /**
  * @description HTML <meta> tags options
  */
 export const meta = {
-  description : "Description will go into a meta tag in <head />",
+  title       : site.title,
+  description : site.description,
+  image       : undefined, // TODO: Meta Image : for linking thumbnail/etc
+  tags        : [],        // TODO: Meta tags  : https://docusaurus.io/docs/api/themes/configuration#metadata
 }
 
 /**
  * @description GitHub repository options
  */
-const ghProjectName = 'etherealengine-docs'
 export const github = {
-  projectName : ghProjectName,
-  editURL     : `https://github.com/EtherealEngine/${ghProjectName}/blob/master/`,
+  username   : org.username,
+  docsName   : GHDocsName,
+  docsURL    : GHDocsURL,
+  editURL    : `${GHDocsURL}/blob/master/`,
+  projectURL : GithubURL,
+}
+
+/**
+ * @description Typedoc API specific options
+ */
+export const typedoc = {
+  label : 'API',
+  url   : site.fullURL+'typedoc',
+}
+
+/**
+ * @description NavBar specific options
+ */
+export const navbar = {
+  menu : {
+    position : 'right',
+  },
+  search: {
+    position : 'left',
+    css      : '/',
+  }
+}
+
+/**
+ * @description Footer specific options
+ */
+export const footer = {
+  iconSize  : 50,
+  copyright : `Copyright © ${new Date().getFullYear()} ${org.title}.`,
+  links     : [
+    { title: 'Social',
+      items: [
+        { label: 'Twitter', href: Links.twitter },
+        { label: 'Facebook', href: Links.facebook },
+        { label: 'Discord', href: Links.discord }
+      ]
+    },
+    { title: 'Resources',
+      items: [
+        { label: 'Github', href: Links.github },
+        { label: 'Npm', href: Links.npm }
+      ]
+    },
+    { title: 'More',
+      items: [
+        { label: org.title, href: Links.orgSite },
+        { label: 'Open Collective', href: Links.openCollective }
+      ]
+    }
+  ]
 }
 
 /**
  * @description Ethereal Engine specific options
  */
-export const visualScriptName :string= 'Behave Graph'
+export const VisualscriptName :string= 'Behave Graph'
+
