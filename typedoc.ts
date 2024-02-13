601a44a7641edd701adbf0256fe89ee5d7539695
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-02-12 19:19:37 -0800

    bld: Change `/typedoc` to `/api`

--- typedoc.ts
+++ typedoc.ts
@@ -6,7 +6,9 @@ const dotenv = require('dotenv').config({
     path: appRootPath.path + '/.env.local'
   })
 
-const typedoc = spawn("npx", ["typedoc", process.env.ENGINE_PATH, "--options", `${process.env.ENGINE_PATH}/typedoc.json`, "--out", "./build/typedoc"])
+import { ApiBaseURL } from './website.config'
+
+const typedoc = spawn("npx", ["typedoc", process.env.ENGINE_PATH, "--options", `${process.env.ENGINE_PATH}/typedoc.json`, "--out", "./build/"+ApiBaseURL])
 
 typedoc.stdout.on("data", data => {
     console.log(`stdout: ${data}`);
