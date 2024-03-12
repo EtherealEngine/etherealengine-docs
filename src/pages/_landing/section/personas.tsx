02f7bb1d47d04250cb350e0477351177f1596f81
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-03-13 00:40:18 +0100

    chg: Add `id` field to the SimplePersonas title div

--- src/pages/_landing/section/personas.tsx
+++ src/pages/_landing/section/personas.tsx
@@ -73,7 +73,7 @@ export function SimplePersonas(props) {
   const PersonasDescr = "font-normal justify-items-center text-left text-xl"
   return <React.Fragment>
     <section className={PersonasContainer}>
-      <div className={PersonasTitle}>{Data.title}</div>
+      <div className={PersonasTitle} id="choose-path">{Data.title}</div>
       <Persona data={Data.manual} />
       <div className={PersonasDescr}>{Data.description}</div>
     </section>
